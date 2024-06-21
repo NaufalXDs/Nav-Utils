4ea1cf684b1eb9a6d0fc0fb7f7c1895e627aa20a
NavDev <naufaldes07@gmail.com>
2024-06-21 19:39:09 +0700

    Update -> "whitelist domain image from Oauth"

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                hostname: 'avatars.githubusercontent.com',
-            },
-        ],
+        domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     },
 };
 
